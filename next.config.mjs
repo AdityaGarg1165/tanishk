6c495c38b2f7572e26fce41db4aa36f946bd5a47
AdityaGarg1165 <90836787+AdityaGarg1165@users.noreply.github.com>
2025-03-30 13:48:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/tanishk',
-    assetPrefix: '/tanishk',
   };
   
   export default nextConfig;
